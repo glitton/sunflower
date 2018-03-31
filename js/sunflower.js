5f625b706a234952954d6746f8ea63088b874159
Generosa Litton <glcodeworks@gmail.com>
2018-03-31 14:34:51 -0700

    added form validation

--- js/sunflower.js
+++ js/sunflower.js
@@ -15,4 +15,30 @@ $(document).ready(function() {
       format: "MM/YYYY"
     });
   });
+
+  // Exercise counseling signup form validation
+  (function() {
+    window.addEventListener(
+      "load",
+      function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName("needs-validation");
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener(
+            "submit",
+            function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add("was-validated");
+            },
+            false
+          );
+        });
+      },
+      false
+    );
+  })();
 }); //end of document.ready
